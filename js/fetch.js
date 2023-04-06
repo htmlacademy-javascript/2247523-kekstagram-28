a2a117ecfd6eb7c254c0f1dfc4d16d22d14d73c7
NatashaTrifonova <natali.flow@yandex.ru>
2023-04-06 20:48:46 +0300

    Надо подкачаться

--- js/fetch.js
+++ js/fetch.js
@@ -18,18 +18,18 @@ const ErrorText = {
 const load = (route, errorText, method = Method.GET, body = null) =>
   fetch(`${BASE_URL}${route}`, {method, body})
     .then((response) => {
-
       if (!response.ok) {
         throw new Error();
       }
       return response.json();
     })
     .catch(() => {
+      showAlert(errorText);
       throw new Error(errorText);
     });
 
 const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
 
-const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST,body);
 
 export { getData, sendData };
